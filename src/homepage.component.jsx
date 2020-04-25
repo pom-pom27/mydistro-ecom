9e2aa43ef6da11d7f253b78aa0d9d4556bc6a00f
selja2798 <siotong2798@gmail.com>
2020-04-26 00:33:44 +0700

    create projet using cra and add homepage barebone

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./homepage.module.scss";
+
+const Homepage = () => {
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.directoryMenu}>
+        <div className={styles.menuItem}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>HATS</h1>
+            <span className={styles.subtitle}>SHOP NOW</span>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>JACKETS</h1>
+            <span className={styles.subtitle}>SHOP NOW</span>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>SNEAKERS</h1>
+            <span className={styles.subtitle}>SHOP NOW</span>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>WOMENS</h1>
+            <span className={styles.subtitle}>SHOP NOW</span>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>MENS</h1>
+            <span className={styles.subtitle}>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Homepage;
